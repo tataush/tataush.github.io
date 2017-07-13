ffc2c6c9adc380a5dae219c988513248f0d94f12
Ivan Tataush <tataushivan@gmail.com>
2017-07-14 00:29:19 +0300

    add calc

---
+++ calc/script.js
@@ -0,0 +1,6 @@
+var table = [
+	{ title: 'Штукатурка', price: 10, type: "м.кв" },
+	{ title: 'Уборка', price: 12, type: "м.пог" },
+	{ title: 'Кладка', price: 14, type: "м.кв" },
+	{ title: 'Покраска', price: 14, type: "м.пог" }
+]
