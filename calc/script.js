ae28fb09a377046fbfda4ea763f8741b9fa5ba72
Ivan Tataush <ivan.tataush@intexsys.lv>
2017-07-14 17:47:23 +0300

    remove all

--- calc/script.js
+++
@@ -1,6 +0,0 @@
-var table = [
-	{ title: 'Штукатурка', price: 10, type: "м.кв" },
-	{ title: 'Уборка', price: 12, type: "м.пог" },
-	{ title: 'Кладка', price: 14, type: "м.кв" },
-	{ title: 'Покраска', price: 14, type: "м.пог" }
-]
