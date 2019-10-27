f137698cb664a5cd55a36062f26f1524bcd843a1
Ivan Tataush <tataushivan@gmail.com>
2019-10-27 21:16:22 +0200

    checkout pages

--- js/main.js
+++ js/main.js
@@ -37,14 +37,34 @@ $(document).ready(function(){
 		$(this).closest('.search-block').removeClass('error');
 	});
 
-  	$(document).mouseup(function (e){
+  $(document).mouseup(function (e){
 		var search_block = $(".search-block");
 		if (!search_block.is(e.target) && search_block.has(e.target).length === 0) {
 			$('.search-block').removeClass('active error');
 			$('.search-block input').val('');
 		}
 	});
+
 	$('.mobile-nav').on('click', function(){
 		$('.header_nav').toggleClass('active');
 	})
+
+	$('.count-buttons .product-count-btn').on('click', function() {
+		var input = $('input.product-amount');
+		var amount = parseFloat(input.val());
+		if (amount) {
+			if ($(this).hasClass('more-product')) {
+				input.val(amount += 1);
+			} else {
+				input.val(amount > 1 ? amount -= 1 : 1);
+			}
+		} else {
+			input.val(1);
+		}
+	});
+
+	if ($('input[type="phone"]').length) {
+		console.log('phone')
+		$('input[type="phone"]').mask('+38000-000-00-00');
+	}
 });
