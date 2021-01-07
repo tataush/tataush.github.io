5a320a7960b69d8c20eb638e001c3fc1c82f44cd
tataushivan <tataushivan@gmail.com>
2021-01-08 01:08:32 +0200

    test app

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-app-bar app dark color="primary">
+      <div class="d-flex align-center">
+        <span>Test App</span>
+      </div>
+    </v-app-bar>
+    <v-main>
+      <List/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import List from './components/List';
+
+export default {
+  components: {
+    List
+  }
+};
+</script>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="list-item">
+      <v-card style="width: 100%">
+        <div class="list-item-content">
+          <div
+            style="width: 100%;"
+            @dblclick="edit"
+          >
+            {{ item }}
+          </div>
+          <div>
+            <v-btn
+              fab
+              depressed
+              x-small
+              color="error"
+              @click="remove"
+            >
+              <v-icon dark>
+                mdi-delete
+              </v-icon>
+            </v-btn>
+          </div>
+        </div>
+      </v-card>
+    </div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    item: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    edit() {
+      this.$emit('edit')
+    },
+    remove() {
+      this.$emit('remove')
+    }
+  }
+}
+</script>
+
+
+<style>
+.list-item {
+  border: 1px solid #ccc;
+  margin-bottom: 8px;
+  width: 100%;
+  cursor: move;
+}
+
+.list-item-content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+}
+</style>
